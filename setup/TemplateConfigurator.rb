1ca41d41cbd9af602ae70f7454dacef7115e0e59
Kevin Bradley <kevin.w.bradley@me.com>
2021-09-04 16:14:30 -0700

    potentially add tvOS support

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -70,11 +70,20 @@ module Pod
     def run
       @message_bank.welcome_message
 
-      platform = self.ask_with_answers("What platform do you want to use?", ["iOS", "macOS"]).to_sym
+      platform = self.ask_with_answers("What platform do you want to use?", ["iOS", "tvOS", "macOS"]).to_sym
 
       case platform
         when :macos
           ConfigureMacOSSwift.perform(configurator: self)
+        when: tvos
+          framework = self.ask_with_answers("What language do you want to use?", ["Swift", "ObjC"]).to_sym
+          case framework
+            when :swift
+              ConfigureTVOSSwift.perform(configurator: self)
+
+            when :objc
+              ConfigureTVOS.perform(configurator: self)
+          end
         when :ios
           framework = self.ask_with_answers("What language do you want to use?", ["Swift", "ObjC"]).to_sym
           case framework
